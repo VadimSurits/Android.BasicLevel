f1d27b5b3c913d924f5b8451d87dee165917f81d
VadimSurits <Sourits-moscow@mail.ru>
2020-08-21 12:40:22 +1000

    Part of HomeWork 3 Finished

--- app/src/main/java/geekbrains/AndroidBasicLevel/MainActivity.java
+++ app/src/main/java/geekbrains/AndroidBasicLevel/MainActivity.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +22,13 @@ public class MainActivity extends AppCompatActivity {
         final TextView cityName = findViewById(R.id.cityName);
         final TextView temperature = findViewById(R.id.temperature);
         final ImageView mainImage = findViewById(R.id.mainImage);
+
+        String instanceState;
+        if(savedInstanceState == null){
+            instanceState = "Первый запуск";
+        } else{
+            instanceState = "Повторный запуск";
+        }
 
         cityName.setText(R.string.cityName);
         temperature.setText(R.string.temperature);
@@ -41,5 +50,63 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        Toast.makeText(getApplicationContext(), instanceState + " MainActivity - onCreate()", Toast.LENGTH_SHORT).show();
+        Log.d("MainActivity", " onCreate()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(getApplicationContext()," MainActivity - onStart()", Toast.LENGTH_SHORT).show();
+        Log.d("MainActivity", " onStart()");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle saveIS){
+        super.onRestoreInstanceState(saveIS);
+        Toast.makeText(getApplicationContext(), "Повторный запуск MainActivity - onRestoreInstanceState()", Toast.LENGTH_SHORT).show();
+        Log.d("MainActivity", " Повторный запуск MainActivity - onRestoreInstanceState())");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Toast.makeText(getApplicationContext(), " MainActivity - onResume()", Toast.LENGTH_SHORT).show();
+        Log.d("MainActivity", " onResume()");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Toast.makeText(getApplicationContext(), " MainActivity - onPause()", Toast.LENGTH_SHORT).show();
+        Log.d("MainActivity", " onPause()");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle saveIS){
+        super.onSaveInstanceState(saveIS);
+        Toast.makeText(getApplicationContext(), " MainActivity - onSaveInstanceState()", Toast.LENGTH_SHORT).show();
+        Log.d("MainActivity", " onSaveInstanceState()");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Toast.makeText(getApplicationContext(), " MainActivity - onStop()", Toast.LENGTH_SHORT).show();
+        Log.d("MainActivity", " onStop()");
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Toast.makeText(getApplicationContext(), " MainActivity - onRestart()", Toast.LENGTH_SHORT).show();
+        Log.d("MainActivity", " onRestart()");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Toast.makeText(getApplicationContext(), " MainActivity - onDestroy()", Toast.LENGTH_SHORT).show();
+        Log.d("MainActivity", " onDestroy()");
     }
 }
--- app/src/main/java/geekbrains/AndroidBasicLevel/SettingsActivity.java
+++ app/src/main/java/geekbrains/AndroidBasicLevel/SettingsActivity.java
@@ -3,6 +3,8 @@ package geekbrains.AndroidBasicLevel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -10,5 +12,70 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        String instanceState;
+        if(savedInstanceState == null){
+            instanceState = "Первый запуск";
+        } else{
+            instanceState = "Повторный запуск";
+        }
+        Toast.makeText(getApplicationContext(), instanceState + " SettingsActivity - onCreate()", Toast.LENGTH_SHORT).show();
+        Log.d("SettingsActivity", " onCreate()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(getApplicationContext()," SettingsActivity - onStart()", Toast.LENGTH_SHORT).show();
+        Log.d("SettingsActivity", " onStart()");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle saveIS){
+        super.onRestoreInstanceState(saveIS);
+        Toast.makeText(getApplicationContext(), "Повторный запуск SettingsActivity - onRestoreInstanceState()", Toast.LENGTH_SHORT).show();
+        Log.d("SettingsActivity", " Повторный запуск SettingsActivity - onRestoreInstanceState()");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Toast.makeText(getApplicationContext(), " SettingsActivity - onResume()", Toast.LENGTH_SHORT).show();
+        Log.d("SettingsActivity", " onResume()");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Toast.makeText(getApplicationContext(), " SettingsActivity - onPause()", Toast.LENGTH_SHORT).show();
+        Log.d("SettingsActivity", " onPause()");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle saveIS){
+        super.onSaveInstanceState(saveIS);
+        Toast.makeText(getApplicationContext(), " SettingsActivity - onSaveInstanceState()", Toast.LENGTH_SHORT).show();
+        Log.d("SettingsActivity", " onSaveInstanceState()");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Toast.makeText(getApplicationContext(), " SettingsActivity - onStop()", Toast.LENGTH_SHORT).show();
+        Log.d("SettingsActivity", " onStop()");
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Toast.makeText(getApplicationContext(), " SettingsActivity - onRestart()", Toast.LENGTH_SHORT).show();
+        Log.d("SettingsActivity", " onRestart()");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Toast.makeText(getApplicationContext(), " SettingsActivity - onDestroy()", Toast.LENGTH_SHORT).show();
+        Log.d("SettingsActivity", " onDestroy()");
     }
 }
