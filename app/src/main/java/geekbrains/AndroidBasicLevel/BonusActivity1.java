dba0fe2c6acc882fc8f6faf47158c62b967e2776
VadimSurits <Sourits-moscow@mail.ru>
2020-08-28 19:48:30 +1000

    1 Attempt of HomeWork 5

---
+++ app/src/main/java/geekbrains/AndroidBasicLevel/BonusActivity1.java
@@ -0,0 +1,14 @@
+package geekbrains.AndroidBasicLevel;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BonusActivity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bonus1);
+    }
+}
