2c0b26cc8b8f730c03383118f9e2e35884725726
VadimSurits <Sourits-moscow@mail.ru>
2020-09-14 20:49:34 +1000

    Не получается справится со 2 заданием. Изначально пытаюсь выполнить это задание на основе учебного приложения, шаг за шагом выполняя те же действия (с учетом особенностей именно моего проекта), в какой-то момент нужное Activity даже запускалось визуально корректно (но нажатия не отрабатывались), после чего без значительных изменений с моей стороны Activity перестало запускаться, постоянно вылетает ошибка при создании Activity: java.lang.RuntimeException: Unable to start activity ComponentInfo{geekbrains.AndroidBasicLevel/geekbrains.AndroidBasicLevel.PreviousRequests.PreviousRequestsActivity}: android.view.InflateException: Binary XML file line #82: Binary XML file line #82: Error inflating class com.google.android.material.navigation.NavigationView. Пробую сделать данный коммит, после чего откатиться к предыдущему и начать все сначала.

--- app/src/main/java/geekbrains/AndroidBasicLevel/PreviousRequests/PreviousRequestsActivity.java
+++ app/src/main/java/geekbrains/AndroidBasicLevel/PreviousRequests/PreviousRequestsActivity.java
@@ -1,16 +1,28 @@
 package geekbrains.AndroidBasicLevel.PreviousRequests;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.navigation.NavigationView;
+
 import java.util.ArrayList;
 
+import geekbrains.AndroidBasicLevel.CityChoiceActivity;
+import geekbrains.AndroidBasicLevel.MainActivity;
 import geekbrains.AndroidBasicLevel.R;
+import geekbrains.AndroidBasicLevel.SettingsActivity;
 
-public class PreviousRequestsActivity extends AppCompatActivity {
+public class PreviousRequestsActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private RecyclerView recyclerView;
     private RecyclerDataAdapter_for_PRActivity recyclerDataAdapter_for_prActivity;
@@ -31,6 +43,50 @@ public class PreviousRequestsActivity extends AppCompatActivity {
         recyclerDataAdapter_for_prActivity = new RecyclerDataAdapter_for_PRActivity(previousRequests);
         recyclerView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
         recyclerView.setAdapter(recyclerDataAdapter_for_prActivity);
+
+        Toolbar toolbar = initToolbar();
+        initDrawer(toolbar);
     }
 
+    private Toolbar initToolbar(){
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        return toolbar;
+    }
+
+    private void initDrawer(Toolbar toolbar){
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view2);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case R.id.nav_mainActivity:
+                startActivity(new Intent(PreviousRequestsActivity.this, MainActivity.class));
+            case R.id.nav_cityChoiceActivity:
+                startActivity(new Intent(PreviousRequestsActivity.this, CityChoiceActivity.class));
+            case R.id.nav_settingsActivity:
+                startActivity(new Intent(PreviousRequestsActivity.this, SettingsActivity.class));
+        }
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        } else{
+            super.onBackPressed();
+        }
+    }
 }
